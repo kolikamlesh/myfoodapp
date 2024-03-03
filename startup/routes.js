5260b08e83e89752ff135bde8bbe50a71ec55c97
kamlesh <45515891+kolikamlesh@users.noreply.github.com>
2024-03-03 11:24:12 +0530

    Add files via upload

---
+++ startup/routes.js
@@ -0,0 +1,36 @@
+const home = require('../routes/home');
+const join = require('../routes/join');
+const error = require('../middleware/error');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const record = require('../routes/record');
+
+
+module.exports = function(app, express) {
+   
+    app.use(cookieParser('keyboard cat'));
+    app.use(session({ 
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 60000 }}
+    ));
+
+    //standard middleware for parsing json request
+    app.use(express.json());
+
+    //form data parsing
+    app.use(express.urlencoded({extended:true}));
+
+    // rendering public static files
+    app.use('/', express.static('public'));
+
+    //set view engine ejs
+    app.set('view engine', 'ejs');
+
+    // route
+    app.use('/', home);
+    app.use('/join/', join);
+    app.use('/secret/', record);
+    app.use(error);
+}
